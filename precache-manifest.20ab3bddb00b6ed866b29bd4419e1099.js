b16be776f5ae66f30fd82c5299d056e7284bf22c
eugene <gagarin126rus@mail.ru>
2020-04-15 14:20:10 +0300

    Updates

---
+++ precache-manifest.20ab3bddb00b6ed866b29bd4419e1099.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8a34056017991ef1e7748ce337d542",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "81ea90ed5ee0dcc12d34",
+    "url": "/MovieApp/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "b9171a683c868254b74e",
+    "url": "/MovieApp/static/css/main.ab93e95b.chunk.css"
+  },
+  {
+    "revision": "81ea90ed5ee0dcc12d34",
+    "url": "/MovieApp/static/js/2.50f0957e.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/MovieApp/static/js/2.50f0957e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9171a683c868254b74e",
+    "url": "/MovieApp/static/js/main.7017c140.chunk.js"
+  },
+  {
+    "revision": "1d19d2423c6f5192923a",
+    "url": "/MovieApp/static/js/runtime-main.84ffd70d.js"
+  }
+]);
